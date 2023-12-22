2afd05a510e2930cb335d930a46d342a4c5018d5
an0de <149331026+an0de@users.noreply.github.com>
2023-12-22 17:20:50 +0200

    add gcd alg

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,10 +1,16 @@
 import { getRandomNumber } from '../utils.js';
 
-const gcd = (number) => number;
+const gcd = (a, b) => {
+  while (b !== 0) {
+  [a, b] = [b, a % b]
+  }
+  return a
+}
 
 const gcdGame = (maxNumber = 100) => {
-  const number = getRandomNumber(maxNumber);
-  const correctAnswer = gcd(number);
-  return [number, correctAnswer];
+  const a = getRandomNumber(maxNumber);
+  const b = getRandomNumber(maxNumber);
+  const correctAnswer = gcd(a, b);
+  return [`$[a} ${b}`, correctAnswer];
 };
 export default gcdGame;
